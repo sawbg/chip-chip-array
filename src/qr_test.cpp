4409881418aca1e3aa81815274c9fdfabd1f866e
Andrew Wisner <awisner94@gmail.com>
2016-01-19 13:14:55 +0000

    Finished.

--- src/qr_test.cpp
+++ src/qr_test.cpp
@@ -1,12 +1,41 @@
-/*#include <iostream>
+#include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <string>
 
 #include "definitions.hpp"
 #include "PiCamera.hpp"
+#include "ScanQR.hpp"
+
+using namespace ChipChipArray;
 
 /**
  *
  */
-int main()
-	*/
+int main() {
+	while(true) {
+		Color color = ScanQR();
+		std::string colstr;
+
+		switch(color) {
+			case Color::Red:
+				colstr = "RED";
+				break;
+
+			case Color::Yellow:
+				colstr = "YELLOW";
+				break;
+
+			case Color::Green:
+				colstr = "GREEN";
+				break;
+
+			case Color::Blue:
+				colstr = "BLUE";
+				break;
+
+		}
+
+		std::cout << colstr << std::endl;
+	}
+}
