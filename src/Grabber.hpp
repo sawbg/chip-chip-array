12c11707606bf148939ee0c252da8171087aaa97
Chip Chip Array <pi@localahost>
2016-03-11 06:37:23 +0500

    More test code and changed indentation.

--- src/Grabber.hpp
+++ src/Grabber.hpp
@@ -57,11 +57,6 @@ namespace ChipChipArray {
 
 		protected:
 			/**
-			 *
-			 */
-			Arm arm;
-			
-			/**
 			 * The Raspicam
 			 */
 			PiCamera cam;
@@ -98,7 +93,7 @@ namespace ChipChipArray {
 			 *
 			 * @param block the block to find
 			 */
-			Block FindBlock(Block block);
+			//			Block FindBlock(Block block);
 
 			/**
 			 * Picks up block by examing how much it fills up the
@@ -128,6 +123,11 @@ namespace ChipChipArray {
 			Block LocateBlocks();
 
 			/**
+			 *
+			 */
+			Block LocateBlocks(Color color);
+
+			/**
 			 * Puts block in slot according to color. Zones B and C
 			 * only.
 			 *
@@ -152,7 +152,7 @@ namespace ChipChipArray {
 			/**
 			 * The minimum area in pixels to be considered a block
 			 */
-			static const uint32 MIN_HALF_BLOCK_SIZE = 50000;
+			static const uint32 MIN_HALF_BLOCK_SIZE = 5000;
 
 			/**
 			 * The log object to be shared among the Grabber
@@ -188,222 +188,234 @@ namespace ChipChipArray {
 		log.Verbose("Zone: " + std::to_string(zone));
 		log.Verbose("Side: " + std::to_string(side));
 
-		invokeCount++;
-
 		this->zone = zone;
 		this->side = side;
 
 		log.Verbose("Setting HSV threshold values");
 
-		rangeVals[Color::Red] = { cv::Scalar(0, 20, 20),
+		rangeVals[Color::Red] = { cv::Scalar(0, 20, 60),
 			cv::Scalar(12, 255, 255) };
-		rangeVals[Color::Green] = { cv::Scalar(49, 0, 0),
-			cv::Scalar(63, 255, 30) };
+		rangeVals[Color::Green] = { cv::Scalar(49, 41, 17),
+			cv::Scalar(63, 255, 255) };
 		rangeVals[Color::Blue] = { cv::Scalar(70, 0, 0),
 			cv::Scalar(100, 255, 255) };
 
 		/* Remember, we're only pretending this color's image is in HSV space.
 		 * It's really in YUV, as required by Jacob yellow-detection algorithm. */
 		rangeVals[Color::Yellow] = { cv::Scalar(0, 0, 0),
-			cv::Scalar(255, 255, 5);
-		}
+			cv::Scalar(255, 255, 40)};
+	}
 
-		void Grabber::Close() {
-			log.Status("Closing Grabber");
-			cam.Close();
-		}
+	void Grabber::Close() {
+		log.Status("Closing Grabber");
+		cam.Close();
+	}
 
-		void Grabber::Discard() {
+	void Grabber::Discard() {
 
-		}
+	}
 
-		void Grabber::Extend() {
+	void Grabber::Extend() {
 
-		}
+	}
 
-		void Grabber::FindBlock(Block block) {
+	/*void Grabber::FindBlock(Block block) {
 
-		}
+	  }*/
 
-		void Grabber::Grab(BlockPosition pos) {
+	void Grabber::Grab(BlockPosition pos) {
 
-		}
+	}
 
-		void Grabber::Grab(Block block) {
-			// assume random base-turn constant of 10
-			while(block.dRightLeft > 20) {
-				arm.dBaseTurn(block.dRightLeft / 10);
-				block = LocateBlocks(blocks);
-			}	
+	void Grabber::Grab(Block block) {
+		// assume random base-turn constant oif 10
+		uint8 max = 0;
+		arm.Grippers(90);
+		
+			while(std::abs(block.dRightLeft) > 20 && max++ < 3) {
+			sint16 baseTurnAngle = (block.dRightLeft > 0 ? -1 : 1)
+				* std::sqrt(std::abs(block.dRightLeft)) / 3 + 0.5;
+			sint16 elbowAngle = (block.dTopBottom > 0 ? -1 : 1)
+				* std::sqrt(std::abs(block.dTopBottom)) * 1  + 0.5; 
 			
+			if(arm.servoPos[BASE_TURN] > 135 
+					&& arm.servoPos[BASE_TURN] < 165)  {
+				arm.dBaseTurn(baseTurnAngle);
+			}
+			
+			log.Debug(string("Elbow anglie: ") + elbowAngle);
+			log.Debug(string("Base turn angle: ") + baseTurnAngle);
+
+			arm.dElbow(elbowAngle);
+			block = LocateBlocks(block.color);
 		}
 
-		Result Grabber::Load() {
-			/*setServoPosition(BASE_TURN, 150);
-			setServoPosition(WRIST_TILT, 50);
-			setServoPosition(ELBOW, 140);
-			sleep(2);
-			Block b = LocateBlocks();
-			int Q = 402;
-			float32 a = 180 / 3.14152 * acos((float)b.offset / Q);
-			if(a > 90) a -= 90;
-			log.Debug("Angle a: " + to_string(a));
-			setServoPosition(BASE_TURN, 150 + a);
-			setServoPosition(WRIST_TILT, 50 - a);
-			setServoPosition(ELBOW, 125);
-			sleep(5);
-			setServoPosition(BASE_TURN, 150);
-			setServoPosition(WRIST_TILT, 50);
-			setServoPosition(ELBOW, 140);
-			*/
-			Grab(LocateBlocks(Color::Perrywinkle);
+
+		arm.Grippers(0);
+
+	}
+
+	Result Grabber::Load() {
+		Grab(LocateBlocks(Color::Perrywinkle));
+	}
+
+	Block Grabber::LocateBlocks(Color color) {
+		invokeCount++;
+		std::string logstr = "Locating blocks";
+
+		if(color == Color::Perrywinkle) {
+			logstr += " (" + std::to_string(color) + ")";
 		}
 
-		Block Grabber::LocateBlocks(Color color) {
-			std::string logstr = "Locating blocks";
+		log.Verbose(logstr);
 
-			if(color == Color::Perrywinkle) {
-				logstr += " (" + std::to_string(color) + ")";
+		cv::Mat imgOrig;
+		cv::transpose(cam.Snap(), imgOrig);
+
+		cv::Mat imgHSV;
+		cv::Mat imgThresh;
+		std::vector<cv::Rect> blocks;
+		std::vector<Color> colors;
+
+		uint8 loopNum = (color == Color::Perrywinkle ? rangeVals.size() : 1);
+
+		for(int i = 0; i < loopNum;	i++) {
+			if(loopNum > 1) {
+				switch(i) {
+					case 0:
+						color = Color::Red;
+						break;
+
+					case 1:
+						color = Color::Green;
+						break;
+
+					case 2:
+						color = Color::Blue;
+						break;
+
+						/* Must be last, because it changes imgHSV from HSV space
+						 * to YUV space. */
+					case 3: 
+						color = Color::Yellow;
+						break;
+				}
 			}
 
-			log.Verbose(logstr);
+			log.Verbose("Searching: " + std::to_string(color));
 
-			cv::Mat imgOrig = cam.Snap();
-			cv::transpose(cam.Snap(), imgOrig);
+			if(color == Color::Yellow) {
+				cv::Mat temp;
+				imgOrig.copyTo(temp);
+				cv::cvtColor(imgOrig, imgHSV, CV_BGR2YUV);
+				cv::cvtColor(imgHSV, temp, CV_HSV2BGR);
+				cv::cvtColor(temp, imgHSV, cv::COLOR_BGR2HSV);
+				log.Image(temp, "yuv_yellow_" + std::to_string(color)
+					+ "_" + std::to_string(zone)
+					+ std::to_string(invokeCount)
+					+ ".bmp");
+			} else {
+				cv::cvtColor(imgOrig, imgHSV, cv::COLOR_BGR2HSV);
+			}
+			
+			cv::inRange(imgHSV, rangeVals[color][0],
+					rangeVals[color][1], imgThresh);
 
-			cv::Mat imgHSV;
-			cv::Mat imgThresh;
-			std::vector<cv::Rect> blocks;
-			std::vector<Color> colors;
+			/* 
+			 * Not quite sure what all this does, but it seems to
+			 * relate to smoothing the image
+			 */
+			cv::erode(imgThresh, imgThresh,
+					cv::getStructuringElement(
+						cv::MORPH_ELLIPSE,
+						cv::Size(5, 5)));
+			cv::dilate(imgThresh, imgThresh,
+					cv::getStructuringElement(
+						cv::MORPH_ELLIPSE,
+						cv::Size(5, 5)));
+			cv::dilate(imgThresh, imgThresh,
+					cv::getStructuringElement(
+						cv::MORPH_ELLIPSE,
+						cv::Size(5, 5)));
+			cv::erode(imgThresh, imgThresh,
+					cv::getStructuringElement(
+						cv::MORPH_ELLIPSE,
+						cv::Size(5, 5)));
 
-			cv::cvtColor(imgOrig, imgHSV, cv::COLOR_BGR2HSV);
+			log.Image(imgThresh, "thresh_" + std::to_string(color)
+					+ "_" + std::to_string(zone)
+					+ std::to_string(invokeCount)
+					+ ".bmp");
 
-			uint8 loopNum = (color == Color::Perrywinkle ? rangeVals.size() : 1);
+			// calculate contours
+			std::vector<std::vector<cv::Point>> contours;
+			cv::findContours(imgThresh, contours, CV_RETR_TREE,
+					CV_CHAIN_APPROX_SIMPLE,
+					cv::Point(0, 0));
+			std::vector<std::vector<cv::Point>>
+				contours_poly(contours.size());
+			std::vector<cv::Rect> bounds(contours.size());
 
-			for(int i = 0; i < loopNum;	i++) {
-				if(color == Color::Perrywinkle) {
-					switch(i) {
-						case 0:
-							color = Color::Red;
-							break;
+			// find rectangle around polygon-ish shapes
+			for(int i = 0; i < contours.size(); i++) {
+				uint32 area = cv::contourArea(contours[i]);
 
-						case 1:
-							color = Color::Green;
-							break;
-
-						case 2:
-							color = Color::Blue;
-							break;
-
-							/* Must be last, because it changes imgHSV from HSV space
-							 * to YUV space. */
-						case 3: 
-							color = Color::Yellow;
-							cv::cvtColor(imgOrig, imgHSV, CV_BGR2YUV);
-							break;
-					}
-
-					log.Verbose("Searching: " + std::to_string(color));
-
-					cv::inRange(imgHSV, rangeVals[color][0],
-							rangeVals[color][1], imgThresh);
-
-					/* 
-					 * Not quite sure what all this does, but it seems to
-					 * relate to smoothing the image
-					 */
-					cv::erode(imgThresh, imgThresh,
-							cv::getStructuringElement(
-								cv::MORPH_ELLIPSE,
-								cv::Size(5, 5)));
-					cv::dilate(imgThresh, imgThresh,
-							cv::getStructuringElement(
-								cv::MORPH_ELLIPSE,
-								cv::Size(5, 5)));
-					cv::dilate(imgThresh, imgThresh,
-							cv::getStructuringElement(
-								cv::MORPH_ELLIPSE,
-								cv::Size(5, 5)));
-					cv::erode(imgThresh, imgThresh,
-							cv::getStructuringElement(
-								cv::MORPH_ELLIPSE,
-								cv::Size(5, 5)));
-
-					log.Image(imgThresh, "thresh_" + std::to_string(color)
-							+ "_" + std::to_string(zone)
-							+ std::to_string(invokeCount)
-							+ ".bmp");
-
-					// calculate contours
-					std::vector<std::vector<cv::Point>> contours;
-					cv::findContours(imgThresh, contours, CV_RETR_TREE,
-							CV_CHAIN_APPROX_SIMPLE,
-							cv::Point(0, 0));
-					std::vector<std::vector<cv::Point>>
-						contours_poly(contours.size());
-					std::vector<cv::Rect> bounds(contours.size());
-
-					// find rectangle around polygon-ish shapes
-					for(int i = 0; i < contours.size(); i++) {
-						cv::approxPolyDP(cv::Mat(contours[i]),
-								contours_poly[i], 20,
-								false);
-						cv::Rect rect = cv::boundingRect(
-								cv::Mat(contours_poly[i]));
-						uint32 area = rect.width * rect.height;
-
-						// determine if block and add to blocks vector
-						if(area > MIN_HALF_BLOCK_SIZE) {
-							log.Debug(std::to_string(color)
-									+ " block detected "
-									"with area "
-									+ std::to_string(
-										area));
-							blocks.push_back(rect);
-							colors.push_back(color);
-						}
-					}
+				// determine if block and add to blocks vector
+				if(area > MIN_HALF_BLOCK_SIZE) {
+					cv::approxPolyDP(cv::Mat(contours[i]),
+							contours_poly[i], 20,
+							false);
+					cv::Rect rect = cv::boundingRect(
+							cv::Mat(contours_poly[i]));
+					log.Debug(std::to_string(color)
+							+ " block detected "
+							"with area "
+							+ std::to_string(
+								area));
+					blocks.push_back(rect);
+					colors.push_back(color);
 				}
-
-				if(blocks.size() == 0) {
-					log.Image(imgOrig, "original_" + std::to_string(zone)
-							+ std::to_string(invokeCount)
-							+ "_no_blocks.bmp");
-					throw std::runtime_error("No blocks found!");
-				} else {
-					log.Status(std::to_string(blocks.size())
-							+ " blocks found");
-				}
-
-				// coordinates start in top right
-				Block block = Block(blocks[0], colors[0]);
-
-				if(blocks.size() > 1) {
-					for(int i = 1; i < blocks.size(); i++) { 
-						if((side == Side::Right && blocks[i].x 
-									> block.topLeft.x)
-								|| (side == Side::Left
-									&& blocks[i].x
-									< block.topLeft.x)) {
-							block = Block(blocks[i], colors[i]);
-						}
-					}
-				}
-
-				log.Status(std::to_string(block.color) + " block is located");
-
-				/* 
-				 * Draw surrounding rectangles from above on original
-				 * image.
-				 */
-				cv::rectangle(imgOrig, block.topLeft , block.bottomRight,
-						cv::Scalar(255, 0, 0), 4, 8);
-				log.Image(imgOrig, "original_" + std::to_string(zone)
-						+ std::to_string(invokeCount)
-						+ ".bmp");
-
-				return block;
 			}
 		}
 
+		if(blocks.size() == 0) {
+			log.Image(imgOrig, "original_" + std::to_string(zone)
+					+ std::to_string(invokeCount)
+					+ "_no_blocks.bmp");
+			throw std::runtime_error("No blocks found!");
+		} else {
+			log.Status(std::to_string(blocks.size())
+					+ " blocks found");
+		}
+
+		// coordinates start in top right
+		Block block = Block(blocks[0], colors[0]);
+
+		if(blocks.size() > 1) {
+			for(int i = 1; i < blocks.size(); i++) { 
+				if((side == Side::Right && blocks[i].x 
+							> block.topLeft.x)
+						|| (side == Side::Left
+							&& blocks[i].x
+							< block.topLeft.x)) {
+					block = Block(blocks[i], colors[i]);
+				}
+			}
+		}
+
+		log.Status(std::to_string(block.color) + " block is located");
+
+		/* 
+		 * Draw surrounding rectangles from above on original
+		 * image.
+		 */
+		cv::rectangle(imgOrig, block.topLeft , block.bottomRight,
+				cv::Scalar(255, 0, 0), 4, 8);
+		log.Image(imgOrig, "original_" + std::to_string(zone)
+				+ std::to_string(invokeCount)
+				+ ".bmp");
+
+		return block;
+	}
+}
 #endif
